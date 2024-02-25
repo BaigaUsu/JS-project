de0e6f6cfabb3e227a4a0427b5ba7b7969f7d98d
BaigaUsu <baigazy7777@gmail.com>
2024-02-25 13:46:21 +0600

    задача 3

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function printStrings(arr, num) {
-  for (let i = 1; i <= num; i++) {
-    arr.forEach(str => {
-      console.log(`${str} ${i}`);
-    });
-  }
-}
+let inputStr = document.getElementById('type')
 
-printStrings(["floor", "vagon"], 3);
+let div = document.querySelector('div')
+
+function moveToDiv() {
+    let splitArray = inputStr.value.split(',').map(item => item.trim()).filter(Boolean);
+    let ol = document.createElement('ol');
+
+    splitArray.forEach(function(item) {
+        let li = document.createElement('li');
+        li.textContent = item;
+        ol.appendChild(li);
+    });
+
+    div.innerHTML = '';
+    div.appendChild(ol);
+};
+
+
+inputStr.addEventListener('input', moveToDiv)
+
