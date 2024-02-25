53de84ccb2e9ad6bf15ed2d464c67db3b34017e5
BaigaUsu <baigazy7777@gmail.com>
2024-02-25 18:45:51 +0600

    initial commit

--- myScript.js
+++ myScript.js
@@ -1,20 +1,10 @@
-'use strict'
 
 let object = {
     name: 'Andrey',
-    say: sayName,
-};
+    };
 
 let object2 = {
     name: 'Ivan',
-    say: sayName
-        
-}
+    }
 
-function sayName() {
-    console.log(this.name)
-}
-
-object.say()
-object2.say()
 
