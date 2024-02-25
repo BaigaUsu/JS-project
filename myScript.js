6c86764c866d925d2b46ea40045caeac50c086a2
BaigaUsu <baigazy7777@gmail.com>
2024-02-25 20:19:14 +0600

    добавил комментарий к 3 задаче

--- myScript.js
+++ myScript.js
@@ -1,10 +1,20 @@
+'use strict'
 
 let object = {
     name: 'Andrey',
-    };
+    say: sayName,
+};
 
 let object2 = {
     name: 'Ivan',
-    }
+    say: sayName
 
+}
 
+function sayName() {
+    console.log(this.name)
+}
+
+object.say()
+object2.say()
+// мы видим разные значения имен благодоря this который дает доступ к(данном случае) имени каждого объекта
